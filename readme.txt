15753322f8e3a23ed7fa0249d97150e46492b8af
artstorm <artstorm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-23 21:43:32 +0000

    Version 1.0 of Post Snippets
    git-svn-id: https://plugins.svn.wordpress.org/post-snippets/tags/1.0@104704 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Post Snippets ===
+Contributors: artstorm
+Donate link: http://coding.cglounge.com/wordpress-plugins/post-snippets/
+Tags: post, admin, snippet, snippets, html, custom, page, dynamic
+Requires at least: 2.7
+Tested up to: 2.7.1
+Stable tag: 1.0
+
+Store snippets of HTML code or reoccurring text that you often use in your posts.
+
+== Description ==
+
+This admin plugin stores snippets of HTML code or reoccurring text that you often use in your posts. You can use predefined variables to replace parts of the snippet on insert. All snippets are available in the post editor with a TinyMCE button.
+
+For complete usage instructions see: [Post Snippets](http://coding.cglounge.com/wordpress-plugins/post-snippets/ "Complete Usage Instructions for Post Snippets") 
+
+
+== Installation ==
+
+   1. Upload the ‘post-snippets’ folder  to the ‘/wp-content/plugins/’ directory.
+   2. Activate the plugin through the ‘Plugins’ menu in WordPress.
+   3. Go to Settings -> Post Snippets and start entering your snippets.
+
+
+== Frequently Asked Questions ==
+
+= What about foo bar? =
+
+Answer to foo bar dilemma.
+
+== Screenshots ==
+
+1. The Admin page where you set up new snippets.
+2. The TinyMCE button for Post Snippet.
+3. The Post Snippet Insert Window.
---
+++ tinymce/window.php
@@ -0,0 +1,118 @@
+<?php
+// look up for the path
+require_once( dirname( dirname(__FILE__) ) .'/post-snippets-config.php');
+
+global $wpdb;
+
+// check for rights
+if ( !is_user_logged_in() || !current_user_can('edit_posts') ) 
+	wp_die(__("You are not allowed to be here"));
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Post Snippets</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<script language="javascript" type="text/javascript" src="<?php echo get_option('siteurl') ?>/wp-includes/js/tinymce/tiny_mce_popup.js"></script>
+	<script language="javascript" type="text/javascript" src="<?php echo get_option('siteurl') ?>/wp-includes/js/tinymce/utils/mctabs.js"></script>
+	<script language="javascript" type="text/javascript" src="<?php echo get_option('siteurl') ?>/wp-includes/js/tinymce/utils/form_utils.js"></script>
+	<script language="javascript" type="text/javascript">
+	function init() {
+		tinyMCEPopup.resizeToInnerSize();
+	}
+	
+	function insertSnippet() {
+		
+		var insertString;
+
+		<?php
+		$snippets = get_option($postSnippets->plugin_options);
+		for ($i = 0; $i < count($snippets); $i++) { ?>
+		var panel<?= $i ?> = document.getElementById('ps_panel<?= $i ?>');
+		<?php }	?>
+
+		var rss = document.getElementById('ps_panel0');
+		
+		<?php
+		$snippets = get_option($postSnippets->plugin_options);
+		for ($i = 0; $i < count($snippets); $i++) {
+			// Make it js safe
+			$theString = str_replace('"','\"',str_replace(Chr(13), '', str_replace(Chr(10), '', $snippets[$i]['snippet'])))
+		?>
+
+		if (panel<?= $i ?>.className.indexOf('current') != -1) {
+			insertString = "<?= $theString; ?>";
+			<?php
+			$var_arr = explode(",",$snippets[$i]['vars']);
+			if (!empty($var_arr[0])) {
+				for ($j = 0; $j < count($var_arr); $j++) { ?>
+					var var_<?= $i ?>_<?= $j ?> = document.getElementById('var_<?= $i ?>_<?= $j ?>').value;
+					insertString = insertString.replace(/{<?= $var_arr[$j] ?>}/g, var_<?= $i ?>_<?= $j ?>);
+			<?php } } ?>
+		}
+		<?php }	?>
+	
+		
+		if(window.tinyMCE) {
+			window.tinyMCE.execInstanceCommand('content', 'mceInsertContent', false, insertString);
+			//Peforms a clean up of the current editor HTML. 
+			//tinyMCEPopup.editor.execCommand('mceCleanup');
+			//Repaints the editor. Sometimes the browser has graphic glitches. 
+			tinyMCEPopup.editor.execCommand('mceRepaint');
+			tinyMCEPopup.close();
+		}
+		
+		return;
+	}
+	</script>
+	<base target="_self" />
+</head>
+<body id="link" onload="tinyMCEPopup.executeOnLoad('init();');document.body.style.display='';" style="display: none">
+<!-- <form onsubmit="insertLink();return false;" action="#"> -->
+	<form name="postSnippets" action="#">
+
+	<div class="tabs">
+		<ul>
+		<?php
+		$snippets = get_option($postSnippets->plugin_options);
+		for ($i = 0; $i < count($snippets); $i++) { ?>
+			<li id="ps_tab<?= $i ?>"<?php if ($i == 0) {?> class="current"><?php } ?><span><a href="javascript:mcTabs.displayTab('ps_tab<?= $i ?>','ps_panel<?= $i ?>');" onmousedown="return false;"><?php echo $snippets[$i]['title']; ?></a></span></li>
+		<?php }	?>
+		</ul>
+	</div>
+	
+	<div class="panel_wrapper">
+    <?php
+	$snippets = get_option($postSnippets->plugin_options);
+	for ($i = 0; $i < count($snippets); $i++) { ?>
+        <div id="ps_panel<?= $i ?>" class="panel<?php if ($i == 0) {?> current<?php } ?>">
+        <br />
+        <table border="0" cellpadding="4" cellspacing="0">
+		<?php
+        $var_arr = explode(",",$snippets[$i]['vars']);
+		if (!empty($var_arr[0])) {
+			for ($j = 0; $j < count($var_arr); $j++) { ?>
+			 <tr>
+				<td nowrap="nowrap"><label for="var_<?= $i ?>_<?= $j ?>"><?php echo($var_arr[$j]);?>:</label></td>
+				<td><input type="text" id="var_<?= $i ?>_<?= $j ?>" name="var_<?= $i ?>_<?= $j ?>" style="width: 190px" />
+				</td>
+			  </tr>
+        <?php } } ?>
+        </table>
+        </div>
+<?php } ?>
+	</div>
+
+	<div class="mceActionPanel">
+		<div style="float: left">
+			<input type="button" id="cancel" name="cancel" value="<?php _e("Cancel"); ?>" onclick="tinyMCEPopup.close();" />
+		</div>
+
+		<div style="float: right">
+			<input type="submit" id="insert" name="insert" value="<?php _e("Insert"); ?>" onclick="insertSnippet();" />
+		</div>
+	</div>
+</form>
+</body>
+</html>
