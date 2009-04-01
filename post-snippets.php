590f54817e6921e492d98ea01e3227bc83ca9aa6
artstorm <artstorm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-01 11:16:04 +0000

    Version 1.2, adds support for Quicktag buttons in the HTML editor
    git-svn-id: https://plugins.svn.wordpress.org/post-snippets/tags/1.2@106923 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ post-snippets.php
@@ -0,0 +1,250 @@
+<?php
+/*
+Plugin Name: Post Snippets
+Plugin URI: http://coding.cglounge.com/wordpress-plugins/post-snippets/
+Description: Stores snippets of HTML code or reoccurring text that you often use in your posts. You can use predefined variables to replace parts of the snippet on insert. All snippets are available in the post editor with a TinyMCE button.
+Version: 1.2
+Author: Johan Steen
+Author URI: http://coding.cglounge.com/
+Text Domain: post-snippets 
+
+Copyright 2009  Johan Steen  (email : artstorm [at] gmail [dot] com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class postSnippets {
+	var $plugin_options = "post_snippets_options";
+
+	function postSnippets()
+	{
+		// define URL
+		define('postSnippets_ABSPATH', WP_PLUGIN_DIR.'/'.plugin_basename( dirname(__FILE__) ).'/' );
+		define('postSnippets_URLPATH', WP_PLUGIN_URL.'/'.plugin_basename( dirname(__FILE__) ).'/' );
+		
+		include_once (dirname (__FILE__)."/tinymce/tinymce.php");
+
+		$this->init_hooks();
+	}
+
+	function init_hooks() {
+		load_plugin_textdomain(	'post-snippets', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+		add_action('admin_menu', array(&$this,'wp_admin'));
+		add_action('admin_footer', array(&$this,'quicktags'));
+	}
+
+	/**
+	* Handling of QuickTags in the HTML editor
+	*
+	*/
+	function quicktags() {
+		$quicktag_pages = array( 'post.php', 'post-new.php', 'page-new.php', 'page.php', 'comment.php' );
+		for($i = 0; $i < count($quicktag_pages); $i++) {
+			if( strpos($_SERVER['REQUEST_URI'], $quicktag_pages[$i]) ) {
+
+				$snippets = get_option($this->plugin_options);
+				if (!empty($snippets)) {
+					echo '
+						<script type="text/javascript">
+							<!--
+							if (postSnippetsToolbar = document.getElementById("ed_toolbar")) {
+								var postSnippetsNr, postSnippetsButton;
+						';
+								for ($i = 0; $i < count($snippets); $i++) {
+
+									// Make it js safe
+									$theSnippet = str_replace('"','\"',str_replace(Chr(13), '', str_replace(Chr(10), '', $snippets[$i]['snippet'])));
+									$var_arr = explode(",",$snippets[$i]['vars']);
+									$theVariables = "";
+									if (!empty($var_arr[0])) {
+										for ($j = 0; $j < count($var_arr); $j++) {
+											$theVariables = $theVariables . "'" . $var_arr[$j] . "'";
+											if ( $j < (count($var_arr) -1) )
+												$theVariables = $theVariables . ", ";
+											
+										}
+									}
+		
+									echo '
+										postSnippetsNr = edButtons.length;
+										edButtons[postSnippetsNr] = new edButton(\'ed_ps'. $i . '\',    \'' . $snippets[$i]['title'] . '\',    \''.$snippets[$i]['snippet'].'\',  \'\',       \'\', -1);
+										var postSnippetsButton = postSnippetsToolbar.lastChild;
+										
+										while (postSnippetsButton.nodeType != 1) {
+											postSnippetsButton = postSnippetsButton.previousSibling;
+										}
+										
+										postSnippetsButton = postSnippetsButton.cloneNode(true);
+										postSnippetsToolbar.appendChild(postSnippetsButton);
+										postSnippetsButton.value = \'' . $snippets[$i]['title'] . '\';
+										postSnippetsButton.title = postSnippetsNr;
+										var variables' . $i .' = new Array('.$theVariables.');
+										postSnippetsButton.onclick = function () {edInsertSnippet(edCanvas, \''.$theSnippet.'\', variables' . $i .', parseInt(this.title));}
+										postSnippetsButton.id = "ed_ps' . $i .'";
+									';
+								}
+						echo '
+							}
+							function edInsertSnippet(myField,theSnippet,theVariables) {
+								var myValue;
+								var insertString;
+								insertString = theSnippet;
+								for (x in theVariables)
+								{
+									myValue = prompt(theVariables[x]);
+									var searchfor = \'{\' + theVariables[x] + \'}\';
+									var re = new RegExp(searchfor, \'g\');
+									insertString = insertString.replace(re, myValue);
+									
+								}
+								theSnippet = insertString;
+								if (theSnippet) {
+									edInsertContent(myField, theSnippet);
+								}
+							}							
+							//-->
+						</script>
+						';
+				}
+				break;
+			}
+		}
+	}
+
+	/**
+	* The Admin Page and all it's functions
+	*
+	*/
+	function wp_admin()	{
+		if (function_exists('add_options_page')) {
+			add_options_page( 'Post Snippets Options', 'Post Snippets', 10, __FILE__, array(&$this, 'options_page') );
+		}
+	}
+
+	function admin_message($message) {
+		if ( $message ) {
+			?>
+			<div class="updated"><p><strong><?php echo $message; ?></strong></p></div>
+			<?php	
+		}
+	}
+
+	function options_page() {
+		// Add a new Snippet		
+		if (isset($_POST['add-snippet'])) {
+			$snippets = get_option($this->plugin_options);
+			if (empty($snippets)) { $snippets = array(); }
+			array_push($snippets, array (
+			    'title' => "Untitled",
+			    'vars' => "",
+			    'snippet' => ""));
+			update_option($this->plugin_options, $snippets);
+			$this->admin_message( __( 'A snippet named Untitled has been added.', 'post-snippets' ) );
+		}
+		
+		// Update Snippets
+		if (isset($_POST['update-post-snippets'])) {
+			$snippets = get_option($this->plugin_options);
+			if (!empty($snippets)) {
+				for ($i=0; $i < count($snippets); $i++) {
+					$snippets[$i]['title'] = trim($_POST[$i.'_title']);
+					$snippets[$i]['vars'] = trim($_POST[$i.'_vars']);
+					$snippets[$i]['snippet'] = trim(stripslashes($_POST[$i.'_snippet']));
+				}
+				update_option($this->plugin_options, $snippets);
+				$this->admin_message( __( 'Snippets have been updated.', 'post-snippets' ) );
+			}
+		}
+
+		// Delete Snippets
+		if (isset($_POST['delete-selected'])) {
+			$snippets = get_option($this->plugin_options);
+			if (!empty($snippets)) {
+				$delete = $_POST['checked'];
+				$newsnippets = array();
+				for ($i=0; $i < count($snippets); $i++) {
+					if (in_array($i,$delete) == false) {
+						array_push($newsnippets,$snippets[$i]);	
+					}
+				}
+				update_option($this->plugin_options, $newsnippets);
+				$this->admin_message( __( 'Selected snippets have been deleted.', 'post-snippets' ) );
+			}
+		}
+?>
+<div class=wrap>
+    <h2>Post Snippets</h2>
+
+	<form method="post" action="">
+	<?php wp_nonce_field('update-options'); ?>
+
+    <div class="tablenav">
+        <div class="alignleft actions">
+            <input type="submit" name="add-snippet" value="<?php _e( 'Add New Snippet', 'post-snippets' ) ?>" class="button-secondary" />
+            <input type="submit" name="delete-selected" value="<?php _e( 'Delete Selected', 'post-snippets' ) ?>" class="button-secondary" />
+        </div>
+    </div>
+    <div class="clear"></div>
+    
+    <table class="widefat fixed" cellspacing="0">
+        <thead>
+        <tr>
+            <th scope="col" class="check-column"><input type="checkbox" /></th>
+            <th scope="col" style="width: 180px;"><?php _e( 'Title', 'post-snippets' ) ?></th>
+            <th scope="col" style="width: 180px;"><?php _e( 'Variables', 'post-snippets' ) ?></th>
+            <th scope="col"><?php _e( 'Snippet', 'post-snippets' ) ?></th>
+        </tr>
+        </thead>
+    
+        <tfoot>
+        <tr>
+            <th scope="col" class="check-column"><input type="checkbox" /></th>
+            <th scope="col"><?php _e( 'Title', 'post-snippets' ) ?></th>
+            <th scope="col"><?php _e( 'Variables', 'post-snippets' ) ?></th>
+            <th scope="col"><?php _e( 'Snippet', 'post-snippets' ) ?></th>
+        </tr>
+        </tfoot>
+    
+        <tbody>
+		<?php 
+		$snippets = get_option($this->plugin_options);
+		if (!empty($snippets)) {
+			for ($i=0; $i < count($snippets); $i++) { ?>
+			<tr class='recent'>
+			<th scope='row' class='check-column'><input type='checkbox' name='checked[]' value='<?= $i ?>' /></th>
+			<td class='row-title'><input type='text' name='<?= $i ?>_title' value='<?= $snippets[$i]['title'] ?>' /></td>
+			<td class='name'><input type='text' name='<?= $i ?>_vars' value='<?= $snippets[$i]['vars'] ?>' /></td>
+			<td class='desc'><textarea name="<?= $i ?>_snippet" class="large-text" rows="3"><?= $snippets[$i]['snippet'] ?></textarea></td>
+			</tr>
+		<?php
+			}
+		}
+		?>
+	    </tbody>
+	</table>
+	<div class="submit">
+		<input type="submit" name="update-post-snippets" value="<?php _e( 'Update Snippets', 'post-snippets' ) ?>"  class="button-primary" /></div>
+	</form>
+	<h3><?php _e( 'Help', 'post-snippets' ); ?></h3>
+	<p><?php _e( '<strong>Title</strong><br/>Give the snippet a title that helps you identify it in the post editor.<br/><br/><strong>Variables</strong><br/>A comma separated list of custom variables you can reference in your snippet.<br/><br/>Example:<br/>url,name<br/><br/><strong>Snippet</strong><br/>This is the block of text or HTML to insert in the post when you select the snippet from the insert button in the TinyMCE panel in the post editor. If you have entered predefined variables you can reference them from the snippet by enclosing them in {} brackets.<br/><br/>Example:<br/>To reference the variables in the example above, you would enter {url} and {name}.<br/><br/>So if you enter this snippet:<br/><i>This is the website of &lt;a href="{url}"&gt;{name}&lt;/a&gt;</i><br/>You will get the option to replace url and name on insert if they are defined as variables.', 'post-snippets' ); ?></p>
+</div>
+<?php
+	}
+}
+
+
+add_action( 'plugins_loaded', create_function( '', 'global $postSnippets; $postSnippets = new postSnippets();' ) );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== Post Snippets ===
+Contributors: artstorm
+Donate link: http://coding.cglounge.com/wordpress-plugins/post-snippets/
+Tags: post, admin, snippet, snippets, html, custom, page, dynamic, editor, quicktag
+Requires at least: 2.7
+Tested up to: 2.7.1
+Stable tag: 1.2
+
+Store snippets of HTML code or reoccurring text that you often use in your posts. Custom variables can be used.
+
+== Description ==
+
+This admin plugin stores snippets of HTML code or reoccurring text that you often use in your posts. You can use predefined variables to replace parts of the snippet on insert. All snippets are available in the post editor with a TinyMCE button.
+
+For complete usage instructions see: [Post Snippets](http://coding.cglounge.com/wordpress-plugins/post-snippets/ "Complete Usage Instructions for Post Snippets") 
+
+Some features:
+
+* **Insert** All defined snippets is inserted from a button directly in the post editor.
+* **Buttons** The snippets are available in the viusal editor with a TinyMCE button and in the HTML editor with quicktag buttons.
+* **Admin** Easy to use administration panel where you can add, edit and remove snippets.
+* **Variables** Each snippet can have as many custom variables as you like, which can be used on insert.
+* **Uninstall** If you delete the plugin from your plugins panel it cleans up all data it has created in the Wordpress database. 
+
+
+== Installation ==
+
+1. Upload the 'post-snippets' folder  to the '/wp-content/plugins/' directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to Settings -> Post Snippets and start entering your snippets.
+
+
+== Frequently Asked Questions ==
+
+= Guru? =
+
+Meditation.
+
+== Screenshots ==
+
+1. The Admin page where you set up new snippets.
+2. The TinyMCE button for Post Snippets.
+3. The Post Snippet Insert Window.
+
+== Changelog ==
+
+= 1.2 =
+ * Added support for Quicktags so the snippets can be made available in the HTML editor as well.
+ 
+= 1.1 =
+ * Included Swedish translation.
+ * Added TextDomain functionality for I18n.
+
+= 1.0 =
+ * Initial Release
