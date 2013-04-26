31ce1fc298c9b74a2b6b08411d2ec13adfa7a398
artstorm <artstorm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-26 04:03:30 +0000

    Removes development version. It now lives on GitHub instead.
    git-svn-id: https://plugins.svn.wordpress.org/post-snippets/trunk@703876 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- unittest/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-$GLOBALS['wp_tests_options'] = array(
-    'active_plugins' => array( 'post-snippets/post-snippets.php' ),
-    'template'       => 'twentyeleven',
-    'stylesheet'     => 'twentyeleven',
-);
-
-require_once('D:/Dropbox/Code/_Tools/UniformServer/www/wordpress.dev/wordpress-tests/bootstrap.php');
